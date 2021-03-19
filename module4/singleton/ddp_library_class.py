bc225d47149cae59f4efb56daaef7ae7697632ca
Martinez Sanchez <memelo.martinez.sanchez@intel.com>
2021-03-18 19:14:28 -0600

    modified code for singleton

--- module4/singleton/ddp_library_class.py
+++ module4/singleton/ddp_library_class.py
@@ -1,18 +1,20 @@
+from __future__ import annotations
+
 class Sesion:
 
     # Aquí guardaremos la única instancia permitida
-    __instancia = None
+    _INSTANCIA = None
 
-    def __init__(self, usuario: str):
-        if Sesion.__instancia == None:
+    def __init__(self, usuario:str):
+        if Sesion._INSTANCIA == None:
             self.usuario = usuario
-            Sesion.__instancia = self
+            Sesion._INSTANCIA = self
         else:
-            print('-W- Ya existe un usuario en la sesión')
+            print('-E- Ya existe un usuario en la sesión')
     
     @classmethod
-    def obtener_sesion(cls):
-        return cls.__instancia
+    def obtener_sesion(cls) -> Session:
+        return cls._INSTANCIA
     
-    def logout(self):
-        Sesion.__instancia = None
+    def logout(self) -> None:
+        Sesion._INSTANCIA = None
--- module4/singleton/ddp_library_decorator.py
+++ module4/singleton/ddp_library_decorator.py
@@ -1,9 +1,14 @@
+# Clase auxiliar que nos permite tener un control de las instancias unicas
+# de cada clase
+class Bucket:
+    INSTANCES = {}
+
+# Definicion del decorador. Es una funcion anidada.
 def singleton(clase):
-    instancias = {}
     def obtener_instancia(*args, **kwargs):
-        if clase not in instancias:
-            instancias[clase] = clase(*args, **kwargs)
-        return instancias[clase]
+        if clase not in Bucket.INSTANCES:
+            Bucket.INSTANCES[clase] = clase(*args, **kwargs)
+        return Bucket.INSTANCES[clase]
     return obtener_instancia
 
 @singleton
@@ -13,10 +18,18 @@ class Sesion:
     
     def __str__(self):
         return self.usuario
+    
+    def logout(self):
+        print(Bucket.INSTANCES)
+        print(self.__class__)
+        del Bucket.INSTANCES[self.__class__]
 
 if __name__ == '__main__':
-    s1 = Sesion('usuario1')
-    s2 = Sesion('usuario1')
+    s1 = Sesion('usuario 1')
+    s1.logout()
+    s2 = Sesion('usuario 2')
+    s3 = Sesion('usuario 3')
 
     print(s1)
     print(s2)
+    print(s3)
