8bde9d50d6c2a9e244a39f4d2be0faf335bf65e7
Martinez Sanchez <memelo.martinez.sanchez@intel.com>
2021-03-18 20:09:12 -0600

    appended a copy example for mutable objects

--- module4/prototype/ddp_library.py
+++ module4/prototype/ddp_library.py
@@ -10,7 +10,7 @@ class FiguraAbstracta(ABC):
         pass
 
     @abstractmethod
-    def deepcopy(self):
+    def deep(self):
         pass
 
 class Figura(FiguraAbstracta):
@@ -35,7 +35,7 @@ class Figura(FiguraAbstracta):
     def clonar(self):
         return copy(self)
     
-    def deepcopy(self):
+    def deep(self):
         return deepcopy(self)
 
 class Rectangulo(Figura):
@@ -45,8 +45,8 @@ class Rectangulo(Figura):
         self.alto = alto
     
     def __copy__(self):
-        print('-I- Personalizamos la copia del rectángulo')
-        return Rectangulo(self.ancho + 1, self.alto + 1)
+        ''' Dunder method usado para personalizar la copia '''
+        return Rectangulo(self.ancho, self.alto)
 
     def area(self):
         return self.ancho * self.alto
@@ -58,3 +58,28 @@ class Circulo(Figura):
     
     def area(self):
         return pi * (self.radio ** 2)
+
+# Usamos el método clonar en el rectángulo
+rect_o = Rectangulo(4, 3)
+
+# Elegimos el tipo de copia
+rect_c = rect_o.clonar()
+
+# Imprimimos las diferencias
+print('-' * 75)
+print(f'{"RECTANGULO":^70}')
+print('-' * 75)
+print(f'{"nivel":^15}{"atributo":<20}{"original":^20}{"copia":^20}')
+print('-' * 75)
+print(f'{"subclase":^15}{"ancho":<20}{rect_o.ancho:^20}{rect_c.ancho:^20}')
+print(f'{"subclase":^15}{"alto":<20}{rect_o.alto:^20}{rect_c.alto:^20}')
+print(f'{"padre":^15}{"id":<20}{rect_o.id:^20}{rect_c.id:^20}')
+print(f'{"padre":^15}{"color":<20}{rect_o.color:^20}{rect_c.color:^20}')
+print('-' * 75)
+
+# Mutable objects
+arreglo_original = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+arreglo_copia = copy(arreglo_original)
+arreglo_original[1][0] = 'X'
+print(arreglo_original)
+print(arreglo_copia)
