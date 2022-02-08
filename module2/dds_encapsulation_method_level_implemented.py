2d8ea8b8a26e88a44ebcd8da3ab50f5e3fc97750
Martinez Sanchez <memelo.martinez.sanchez@intel.com>
2022-02-08 08:52:10 -0600

    Changed encapsulation code

--- module2/dds_encapsulation_method_level_implemented.py
+++ module2/dds_encapsulation_method_level_implemented.py
@@ -1,46 +1,52 @@
-# Aplicación de E-Commerce en donde se tiene un método para
-# calcular el total de la orden con impuestos
+#8 de Febrero
+# Principios del diseño de software (3)
 
 class Producto:
-    def __init__(self, descripcion, cantidad, precio):
+    def __init__(self, descripcion:str, cantidad:int, precio:float) -> None:
         self.descripcion = descripcion
         self.cantidad = cantidad
-        self.precio = precio # Asumiremos un precio en pesos
+        self.precio = precio
 
 class Orden:
-    def __init__(self, pais):
+    def __init__(self, pais:str) -> None:
         self.pais = pais
-        self.ordenes = [] # <- Productos
+        self.productos = []
     
-    def agregar(self, producto):
-        self.ordenes.append(producto)
-
+    def agregar(self, producto:Producto) -> None:
+        ''' Agregamos productos a nuestra orden '''
+        self.productos.append(producto)
+    
     def total_orden(self):
+        ''' Tomando en cuenta los impuestos, calcilams el toral de la orden '''
         total = 0
-        for orden in self.ordenes:
-            total += orden.precio * orden.cantidad
+        for p in self.productos:
+            total += p.precio * p.cantidad
         total += total * self.tasa_impuestos()
         return total
-    
-    # Extraemos el código que determina la tasa de impuestos
+
     def tasa_impuestos(self):
+        ''' Retorna el valor del impuesto a aplicar o levanta un error si no encuentra el país '''
         if self.pais == 'US':
             return 0.07
         elif self.pais == 'MX':
             return 0.20
+        elif self.pais == 'Canada':
+            return 0.01
         else:
-            raise Exception('No se tiene el referencia de impuestos aún.')
+            raise Exception(f'El pais {self.pais} no está definido en el método tasa_impuestos')
 
-victor = Orden('MX')
-victor.agregar(Producto('Blusas azules', 5, 120))
-victor.agregar(Producto('Bolsas Lacoste', 2, 4000))
-victor.agregar(Producto('Reloj Michael Kors', 5, 8000))
 
-print(victor.total_orden())
+if __name__ == '__main__':
+    # Este código solamente se va a ejecutar si lo corremos desde el thread principal
+    # python code.py
 
-hugo = Orden('US')
-hugo.agregar(Producto('Blusas azules', 5, 120))
-hugo.agregar(Producto('Bolsas Lacoste', 2, 4000))
-hugo.agregar(Producto('Reloj Michael Kors', 5, 8000))
+    # main.py
+    # > import code
+    # > from code import Orden
+    # Este código no se corre
+    victor = Orden('Canada')
+    victor.agregar(Producto('Blusas rosas', 2, 120))
+    victor.agregar(Producto('Bolsas', 3, 1500))
+    victor.agregar(Producto('Reloj', 5, 2000))
 
-print(hugo.total_orden())
+    print(victor.total_orden())
