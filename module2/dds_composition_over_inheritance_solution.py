70f0cc634052620547147d770814c25991e2b02f
Martinez Sanchez <memelo.martinez.sanchez@intel.com>
2022-02-15 06:52:39 -0600

    changed code and appended composition UML diagram

--- module2/dds_composition_over_inheritance_solution.py
+++ module2/dds_composition_over_inheritance_solution.py
@@ -1,7 +1,8 @@
+from __future__ import annotations
 from abc import ABC
 
 class Transporte:
-    def __init__(self, motor, chofer):
+    def __init__(self, motor:str, chofer:Chofer):
         """ motor: String
             chofer: Chofer
         """
@@ -16,7 +17,8 @@ class Transporte:
         # Agregación
         self.chofer = chofer
     
-    def entregar(self, destino, carga):
+    def entregar(self, destino:str, carga:str) -> str:
+        ''' Consume los datos de la composición y la agregación'''
         return 'Vamos a entregar una carga de {} kilos a {}.\n * {}\n * {}'.format(
             destino,
             carga,
@@ -24,54 +26,56 @@ class Transporte:
             self.chofer.manejar()
         )
     
-    def __str__(self):
+    def __str__(self) -> str:
+        ''' Retorna la representación en texto del objeto '''
         return 'Es un transporte con {} el cual conduce un {}'.format(
             self.motor,
             self.chofer
         )
 
+# Motores
 class Motor(ABC):
-    def mover(self):
+    def mover(self) -> str:
         return 'Moviéndonos...'    
 
-class Chofer(ABC):
-    def manejar(self):
-        return 'Manejando...'
-
 class MotorCombustion(Motor):
-    def mover(self):
+    def mover(self) -> str:
         return 'Moviéndonos con una máquina de combustión...'
     
-    def __str__(self):
+    def __str__(self) -> str:
         return 'motor de combustión'
     
 class MotorElectrico(Motor):
-    def mover(self):
+    def mover(self) -> str:
         return 'Moviéndonos con una máquina de eléctrica...'
     
-    def __str__(self):
+    def __str__(self) -> str:
         return 'motor de eléctrico'
+
+# Choferes
+class Chofer(ABC):
+    def manejar(self) -> str:
+        return 'Manejando...'
     
 class Robot(Chofer):
-    def manejar(self):
+    def manejar(self) -> str:
         return 'Un robot está manejando...'
     
-    def __str__(self):
+    def __str__(self) -> str:
         return 'robot con autopilot'
 
 class Humano(Chofer):
-    def manejar(self):
+    def manejar(self) -> str:
         return 'Un humano está manejando...'
     
-    def __str__(self):
+    def __str__(self) -> str:
         return 'Humano'
 
-# Creamos el tipo de chofer que manejará el transporte
-chofer = Robot()
+if __name__ == '__main__':
+    # Creamos el tipo de chofer que manejará el transporte
+    chofer = Humano()
 
-# Creamos el objeto transporte
-tesla = Transporte('eléctrico', chofer)
-
-# Impriminos los detalles de nuestro transporte
-print(tesla)
-print(tesla.entregar('TIJ', '200'))
+    # Creamos el objeto transporte
+    tesla = Transporte('eléctrico', chofer)
+    print(tesla)
+    print(tesla.entregar('TIJ', '200'))
--- module2/dds_single_responsability_principle_issue.py
+++ module2/dds_single_responsability_principle_issue.py
@@ -1,27 +1,34 @@
-# Estos asignando responsabilidades adicionales a la clase Empleado
-# Rompemos el principio de responsabilidad única
+# SOLID en el diseño de software
+
+from abc import ABC
 
 class Empleado:
-    def __init__(self, nombre:str):
+    def __init__(self, nombre:str) -> None:
         self._nombre = nombre
         self._tiempos = []
     
-    def obtener_Nombre(self) -> str:
+    def obtener_nombre(self) -> str:
+        ''' '''
         return self._nombre
     
-    def nuevo_tiempo(self, dia:int, horas:int) -> None:
-        self._tiempos.append(f'El dia {dia} trabajó: {horas} horas.')
+    def nuevo_tiempo(self, dia:str, horas:int) -> None:
+        ''' Agrega a la lista de tiempos una nueva entrada '''
+        self._tiempos.append(f'El dia {dia} trabajó {horas} horas')
     
     def imprimir_reporte_tiempos(self) -> str:
+        ''' Desplegamos el reporte de tiempos, usamos el método join '''
         return '\n'.join(self._tiempos)
+    
+    def remover_tiempo(self, texto:str) -> None:
+        ''' Elimina un entrada de la lista de tiempos '''
+        self._tiempos.remove(texto)
 
-    def remover_tiempo(self, posicion:int):
-        pass
-
-victor = Empleado('Víctor Hugo')
-victor.nuevo_tiempo('lunes', 8)
-victor.nuevo_tiempo('martes', 8)
-victor.nuevo_tiempo('miercoles', 6)
-victor.nuevo_tiempo('jueves', 8)
-victor.nuevo_tiempo('viernes', 2)
-print(victor.imprimir_reporte_tiempos())
+# Testing
+if __name__ == '__main__':
+    victor = Empleado('Víctor Hugo')
+    victor.nuevo_tiempo('lunes', 8)
+    victor.nuevo_tiempo('martes', 8)
+    victor.nuevo_tiempo('miercoles', 8)
+    victor.nuevo_tiempo('jueves', 8)
+    victor.nuevo_tiempo('viernes', 2)
+    print(victor.imprimir_reporte_tiempos())
