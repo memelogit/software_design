69487d4a4fb910ab99c745ee21fa9404f547b7d6
Martinez Sanchez <memelo.martinez.sanchez@intel.com>
2022-09-29 06:55:12 -0500

    refactoring factory method and decorators

--- module4/dp_decorators.py
+++ module4/dp_decorators.py
@@ -1,19 +1,22 @@
 import time
 
-def timer(f):
-    def wrapper(*args, **kwargs):
-        comienzo = time.time()
-        retorno = f(*args, **kwargs)
-        total = time.time() - comienzo
-        print('Tiempo: ', total)
+def timer(función:object) -> None:
+    ''' Función decorador que puedo anclar a mis métodos o funciones '''
+    def wrapper(*args:list, **kwargs:dict) -> object:
+        ''' Función encargada del comportamiento de mi decorador '''
+        comienzo:float = time.time()
+        retorno = función(*args, **kwargs)
+        total:float = time.time() - comienzo
+        print(f'Tiempo total de ejecución: {total}')
         return retorno
-    
     return wrapper
 
-# Creamos un decorador para nuestra función
 @timer
-def consumo_tiempo(delta):
-    time.sleep(delta)
-    return delta
+def consumo_tiempo(delta_seg:int):
+    ''' función simple que consume tiempo '''
+    print('Comenzando')
+    time.sleep(delta_seg)
+    print('Terminado')
+    return delta_seg
 
-print('Delta: {} segundos'.format(consumo_tiempo(2)))
+consumo_tiempo(2)
--- module4/factory_method/ddp_library.py
+++ module4/factory_method/ddp_library.py
@@ -1,78 +1,73 @@
+# DISEÑO DE SOFTWARE
+# MODULO 4 - PATRONES DE DISEÑO
+# FACTORY METHOD
+# 26 DE SEPTIEMBRE 2022
+
 from abc import ABC, abstractmethod
 from enum import Enum
 
-# -----------------------------------------------------------------------------
-# 1.- INTERFACE PARA LOS PRODUCTOS
-# -----------------------------------------------------------------------------
+# 1.- Interface para productos
 class Bloque(ABC):
     @abstractmethod
     def colocar(self) -> str:
+        ''' Coloca un nuevo tipo de bloque en el mapa '''
         pass
 
-class Arma(ABC):
-    @abstractmethod
-    def usar(self) -> str:
-        pass
-
-# -----------------------------------------------------------------------------
-# 2.- PRODUTOS CONCRETOS
-# -----------------------------------------------------------------------------
+# 2.- Productos Concretos
 class Piedra(Bloque):
     def colocar(self) -> str:
-        print("-I- Bloque del tipo 'piedra' insertado")
+        ''' Coloca un nuevo tipo de bloque en el mapa '''
+        return 'El bloque del tipo piedra fue insertado'
 
 class Granito(Bloque):
     def colocar(self) -> str:
-        print("-I- Bloque del tipo 'granito' insertado")
+        ''' Coloca un nuevo tipo de bloque en el mapa '''
+        return 'El bloque del tipo granito fue insertado'
 
 class Pasto(Bloque):
     def colocar(self) -> str:
-        print("-I- Bloque del tipo 'pasto' insertado")
+        ''' Coloca un nuevo tipo de bloque en el mapa '''
+        return 'El bloque del tipo pasto fue insertado'
 
 class Horno(Bloque):
     def colocar(self) -> str:
-        print("-I- Bloque del tipo 'horno' insertado")
+        ''' Coloca un nuevo tipo de bloque en el mapa '''
+        return 'El bloque del tipo horno fue insertado'
 
 class CamaPiedra(Bloque):
     def colocar(self) -> str:
-        print("-I- Bloque del tipo 'cama de piedra' insertado")
+        ''' Coloca un nuevo tipo de bloque en el mapa '''
+        return 'El bloque del tipo cama_de_piedra fue insertado'
 
-# -----------------------------------------------------------------------------
-# 3.- FACTORY (CREADOR)
-# -----------------------------------------------------------------------------
+# 3.- Factory (Clase creadora)
 class Mundo:
-    @abstractmethod
     def crear_bloque(self, tipo) -> Bloque:
-        pass
+        ''' Crea un nuevo tipo de bloque desde el inventario de productos '''
+        return tipo.value
 
     def colocar(self, tipo) -> str:
-        bloque = self.crear_bloque(tipo)
-        return bloque.colocar()
-    
-    def minar(self, bloque) -> None:
-        pass
+        ''' Coloca un nuevo tipo de bloque en el mapa '''
+        return self.crear_bloque(tipo).colocar()
 
-# -----------------------------------------------------------------------------
-# 4.- FACTORIES EN CONCRETO
-# -----------------------------------------------------------------------------
+# 4.- Fabricas concretas
 class ModoCreativo(Mundo):
 
     class TipoBloque(Enum):
-        PIEDRA         = Piedra()
-        GRANITO        = Granito()
-        PASTO          = Pasto()
-        CAMA_DE_PIEDRA = CamaPiedra()
-
-    def crear_bloque(self, tipo: TipoBloque) -> Bloque:
-        return tipo.value
+        # name           # value
+        PIEDRA           = Piedra()
+        GRANITO          = Granito()
+        PASTO            = Pasto()
+        CAMA_DE_PIEDRA   = CamaPiedra()
 
 class ModoSupervivencia(Mundo):
-
     class TipoBloque(Enum):
-        PIEDRA         = Piedra()
-        GRANITO        = Granito()
-        PASTO          = Pasto()
-        HORNO          = Horno()
+        PIEDRA           = Piedra()
+        GRANITO          = Granito()
+        PASTO            = Pasto()
+        HORNO            = CamaPiedra()
 
-    def crear_bloque(self, tipo: TipoBloque) -> Bloque:
-        return tipo.value
+if __name__ == '__main__':
+    mundo = ModoCreativo()
+
+    p1 = mundo.crear_bloque(mundo.TipoBloque.PIEDRA)
+    print(p1.colocar())
